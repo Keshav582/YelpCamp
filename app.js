9bd330bdfda01dff392767845253780a715a9a14
Keshav Paliwal <19ucs158@lnmiit.ac.in>
2022-07-23 01:36:48 +0530

    create edit campground

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 require('dotenv').config();
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 const connectDB = async () => {
 	try {
@@ -27,9 +31,29 @@ app.get('/campgrounds', async (req, res) => {
 	res.render('campgrounds/index', { campgrounds });
 });
 
+app.post('/campgrounds', async (req, res) => {
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/new', async (req, res) => {
+	res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+	const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 const startApp = () => {
