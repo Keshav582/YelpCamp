d9422602fd3478979ecf2f800cba6718d1a1cc3e
Keshav Paliwal <19ucs158@lnmiit.ac.in>
2022-07-23 00:21:09 +0530

    connected to DB, vague Campground model

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+require('dotenv').config();
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGOURI);
+		startApp();
+	} catch (e) {
+		console.log('Unable to connect to DB');
+	}
+};
+connectDB();
+
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.listen(3000, () => {
-	console.log('Server running on port 3000');
+app.get('/makecampground', async (req, res) => {
+	const camp = new Campground({ title: 'First Campground' });
+	await camp.save();
+	res.send(camp);
 });
+
+const startApp = () => {
+	app.listen(3000, () => {
+		console.log('Server running on port 3000');
+	});
+};
